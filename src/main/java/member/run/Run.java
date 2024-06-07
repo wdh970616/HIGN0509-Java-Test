7c6d2561698e18d2a2ce0493f3538964d9b7c663
wdh970616 <wdh970616@naver.com>
2024-06-07 17:58:05 +0900

    Run 클래스 작성
    문제 예제 대입 및 작동 확인

--- src/main/java/member/run/Run.java
+++ src/main/java/member/run/Run.java
@@ -1,4 +1,24 @@
 package member.run;
 
+import member.controller.MemberRepository;
+import member.model.vo.Gold;
+import member.model.vo.Silver;
+import member.model.vo.Vip;
+
 public class Run {
+    public static void main(String[] args) {
+        MemberRepository memberRepository = new MemberRepository();
+        memberRepository.insertMember(new Silver("홍길동", "Silver", 1000));
+        memberRepository.insertMember(new Silver("고길동", "Silver", 2000));
+        memberRepository.insertMember(new Silver("홍동민", "Silver", 3000));
+        memberRepository.insertMember(new Gold("김회장", "Gold", 1000));
+        memberRepository.insertMember(new Gold("이회장", "Gold", 2000));
+        memberRepository.insertMember(new Gold("오회장", "Gold", 3000));
+        memberRepository.insertMember(new Vip("이순신", "Vip", 10000));
+
+
+        System.out.println("=========================== 회원 포인트 적립 정보 ===========================");
+        memberRepository.printData();
+
+    }
 }
