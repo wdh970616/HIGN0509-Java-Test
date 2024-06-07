94bec753330416f1691b45a6feba4c15cd03d559
wdh970616 <wdh970616@naver.com>
2024-06-07 17:23:14 +0900

    MemberRepository 클래스 작성
    1. MemberList
    2. 기본생성자
    3. insertMember 메소드

--- src/main/java/member/controller/MemberRepository.java
+++ src/main/java/member/controller/MemberRepository.java
@@ -1,4 +1,18 @@
 package member.controller;
 
+import member.model.vo.Member;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MemberRepository {
+
+    private List<Member> memberList = new ArrayList<>(40);
+
+    public MemberRepository() {
+    }
+
+    public void insertMember(Member m) {
+        memberList.add(m);
+    }
 }
